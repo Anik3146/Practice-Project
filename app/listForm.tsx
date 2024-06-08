bc88cc3e5a8a3031d3f973710d25e0af6cd74be8
RTC-03\User <rtc.anik@gmail.com>
2024-06-08 15:37:00 +0600

    complete crud operation in single page in frontend

--- app/listForm.tsx
+++ app/listForm.tsx
@@ -2,14 +2,55 @@ import { useEffect, useState } from "react";
 import { Button, Card, Col, Row, Table } from "react-bootstrap";
 
 export default function ListForm({ obj, setObj }: any) {
-    const handleDelete = (index: number) => {
-        const updatedObj = [...obj];
-        updatedObj.splice(index, 1);
-        setObj(updatedObj);
+    const [editIndex, setEditIndex] = useState(-1);
+    const [tempChanges, setTempChanges] = useState<{ [key: number]: any }>({});
+
+    const handleEdit = (index: number) => {
+        if (editIndex === index) {
+            setEditIndex(-1);
+        } else {
+            setEditIndex(index);
+            setTempChanges(obj[index]);
+        }
     };
-    useEffect(() => {
-        console.log(obj);
-    }, [obj]);
+
+    const handleDelete = (index: number) => {
+        if (editIndex == -1) {
+            const updatedObj = [...obj];
+            updatedObj.splice(index, 1);
+            setObj(updatedObj);
+        } else {
+            setEditIndex(-1);
+        }
+    };
+
+    const handleSave = (index: number) => {
+        setEditIndex(-1);
+    };
+
+    const handleCancel = (index: any) => {
+        setObj((prev: typeof obj) => {
+            obj[index] = tempChanges;
+            return obj;
+        });
+
+        setEditIndex(-1);
+    };
+
+    const handleInputChange = (
+        e: React.ChangeEvent<HTMLInputElement>,
+        index: number,
+        propertyName: string
+    ) => {
+        const { value } = e.target;
+        setObj((prevObj: typeof obj) => {
+            const updatedObj = [...prevObj];
+            updatedObj[index] = { ...updatedObj[index], [propertyName]: value };
+            return updatedObj;
+        });
+    };
+
+    useEffect(() => {}, [tempChanges]);
 
     return (
         <>
@@ -37,25 +78,89 @@ export default function ListForm({ obj, setObj }: any) {
                         <tbody>
                             {obj.map((item: any, index: any) => (
                                 <tr key={index}>
-                                    <td>{item.nameFirst}</td>
-                                    <td>{item.nameLast}</td>
-                                    <td>{item.email}</td>
                                     <td>
-                                        <Button
-                                            variant="secondary"
-                                            title="Edit"
-                                            data-toggle="tooltip"
-                                        >
-                                            Edit
-                                        </Button>
-                                        {/* Divider gap */}
+                                        {editIndex === index ? (
+                                            <input
+                                                type="text"
+                                                value={item.nameFirst}
+                                                name="nameFirst"
+                                                onChange={(e) =>
+                                                    handleInputChange(
+                                                        e,
+                                                        index,
+                                                        "nameFirst"
+                                                    )
+                                                }
+                                            />
+                                        ) : (
+                                            <span>{item.nameFirst}</span>
+                                        )}
+                                    </td>
+                                    <td>
+                                        {editIndex === index ? (
+                                            <input
+                                                type="text"
+                                                value={item.nameLast}
+                                                onChange={(e) =>
+                                                    handleInputChange(
+                                                        e,
+                                                        index,
+                                                        "nameLast"
+                                                    )
+                                                }
+                                            />
+                                        ) : (
+                                            <span>{item.nameLast}</span>
+                                        )}
+                                    </td>
+                                    <td>
+                                        {editIndex === index ? (
+                                            <input
+                                                type="text"
+                                                value={item.email}
+                                                onChange={(e) =>
+                                                    handleInputChange(
+                                                        e,
+                                                        index,
+                                                        "email"
+                                                    )
+                                                }
+                                            />
+                                        ) : (
+                                            <span>{item.email}</span>
+                                        )}
+                                    </td>
+                                    <td>
+                                        {editIndex === index ? (
+                                            <>
+                                                <Button
+                                                    variant="success"
+                                                    title="Save"
+                                                    onClick={() =>
+                                                        handleSave(index)
+                                                    }
+                                                >
+                                                    Save
+                                                </Button>
+                                            </>
+                                        ) : (
+                                            <Button
+                                                onClick={() =>
+                                                    handleEdit(index)
+                                                }
+                                                variant="secondary"
+                                                title="Edit"
+                                                data-toggle="tooltip"
+                                            >
+                                                Edit
+                                            </Button>
+                                        )}
                                         <div
                                             style={{
                                                 width: "10px",
                                                 display: "inline-block",
                                             }}
                                         ></div>
-                                        {/* Divider */}
                                         <hr
                                             style={{
                                                 height: "1px",
@@ -66,14 +171,29 @@ export default function ListForm({ obj, setObj }: any) {
                                                 display: "inline-block",
                                             }}
                                         />
-                                        {/* Delete button */}
                                         <Button
-                                            onClick={() => handleDelete(index)}
-                                            variant="danger"
-                                            title="Delete"
+                                            onClick={() => {
+                                                if (editIndex === index) {
+                                                    handleCancel(index);
+                                                } else {
+                                                    handleDelete(index);
+                                                }
+                                            }}
+                                            variant={
+                                                editIndex === index
+                                                    ? "secondary"
+                                                    : "danger"
+                                            }
+                                            title={
+                                                editIndex === index
+                                                    ? "Cancel"
+                                                    : "Delete"
+                                            }
                                             data-toggle="tooltip"
                                         >
-                                            Delete
+                                            {editIndex === index
+                                                ? "Cancel"
+                                                : "Delete"}
                                         </Button>
                                     </td>
                                 </tr>
--- app/page.tsx
+++ app/page.tsx
@@ -78,6 +78,7 @@ export default function Page() {
 
     useEffect(() => {
         handleClick("list item");
+        console.log(obj);
     }, [obj]);
 
     return (
